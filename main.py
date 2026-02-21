97ff3decfb1d85172d7420c64c87321401fe898c
TumeloMasemola <masemolatumelo486@gmail.com>
2026-02-21 11:06:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import numpy as np
 import pandas as pd
 from datetime import datetime, timedelta
 
-app = FastAPI(title="TerraxIQ API")
+app = FastAPI(title="CommodityIQ API")
 
 app.add_middleware(
     CORSMiddleware,
